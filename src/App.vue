0e0f67fccec40b22117ca3391331ffc97e96a677
Flynn <ppstream123@126.com>
2022-07-18 22:26:19 +0800

    ✨ feat: computed

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,89 @@
 <template>
-	<span>foo:{{ foo }}</span><br>
-	<span>bar:{{ bar }}</span><br>
-	<span>origin:{{ obj }}</span><br>
-	<span>reference:{{ state }}</span><br>
-	<button @click="change">Change</button>
+	<div>
+		<input type="text" name="" id="" v-model="firstName">FirstName:{{ firstName }} <br>
+		<input type="text" name="" id="" v-model="lastName">LastName:{{ lastName }} <br>
+
+		<h2>Hello, {{ fullName }}</h2>
+
+		<h2>Bojour, {{ fullNameCapital }}</h2>
+
+		<table border="1">
+			<thead>
+				<th>Title</th>
+				<th>Quantity</th>
+				<th>Price</th>
+				<th>Operation</th>
+			</thead>
+			<tbody>
+				<tr :key="index" v-for="(item, index) in data">
+					<td align="center">{{ item.name }}</td>
+					<td align="center"><button @click="addAndSub(item, false)">-</button>{{ item.num }}<button
+							@click="addAndSub(item, true)">+</button></td>
+					<td align="center">{{ item.price }}</td>
+					<td align="center"><button @click="del(index)">Delete</button></td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<td>Total Price: {{ total_price }}</td>
+			</tfoot>
+		</table>
+	</div>
 </template>
 <script setup lang="ts">
-import { toRef, triggerRef, reactive, toRefs, toRaw } from 'vue';
-const obj = {
-	foo: 'Hello World',
-	bar: 1
-}
-const obj_reactive = reactive({
-	foo: 'Hello World',
-	bar: 1
-})
-console.log('obj_reactive', obj_reactive)
-const state = toRef(obj_reactive, 'bar')
+import { computed, ref, reactive } from 'vue'
+const firstName = ref('')
+const lastName = ref('')
 
-const obj_raw = toRaw(obj_reactive)
-console.log('obj_raw', obj_raw) // -> obj_raw {foo: 'Hello World', bar: 1}
-const { foo, bar } = toRefs(obj_reactive)
-const change = () => {
-	state.value++
-	foo.value = 'Hello JavaScript'
-	console.log('origin->', obj)
-	console.log('reference->', state)
+const fullName = computed(() => {
+	return firstName.value + ' ' + lastName.value
+})
+const fullNameCapital = computed({
+	get() {
+		return (firstName.value + ' ' + lastName.value).toUpperCase()
+	},
+	set() {
+		lastName.value + lastName.value
+	}
+})
+type Product = {
+	name: string
+	num: number
+	price: number
 }
+const data = reactive<Product[]>([
+	{ name: 'KiKi Li', num: 1, price: 322.3 },
+	{ name: 'CoCo Fit', num: 3, price: 14.8 },
+	{ name: 'Jubilee Lee', num: 2, price: 42.3 },
+])
+
+let total_price = ref(0)
+
+const addAndSub = (item: Product, type: boolean): void => {
+	if (item.num > 1 && !type) {
+		item.num--
+	}
+	if (item.num < 10 && type) {
+		item.num++
+	}
+	//calcTotal()
+}
+
+
+const del = (index: number) => {
+	data.splice(index, 1)
+	//calcTotal()
+}
+const calcTotal = () => {
+	total_price.value = data.reduce((prev, next) => {
+		return prev + next.num * next.price
+	}, 0)
+}
+total_price = computed<number>(() => {
+	return total_price.value = data.reduce((prev, next) => {
+		return prev + next.num * next.price
+	}, 0)
+})
+//calcTotal()
 </script>
 
 
