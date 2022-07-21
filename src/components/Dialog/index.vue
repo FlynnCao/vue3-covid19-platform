4fd1e5faebc17976ee6200092a653ad0b94694d9
Flynn <ppstream123@126.com>
2022-07-21 19:17:18 +0800

    ✨ feat: components ended

---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,34 @@
+import { reactive } from 'vue';
+<template>
+	<div>
+		<header class="header"></header>
+		<main class="main"></main>
+		<footer class="footer"></footer>
+	</div>
+</template>
+	
+<script setup lang='ts'>
+import { reactive } from 'vue';
+
+
+</script>
+	
+<style lang="scss" scoped>
+.header {
+	height: 200px;
+	background: red;
+	color: #fff;
+}
+
+.main {
+	height: 300px;
+	background: green;
+	color: #fff;
+}
+
+.footer {
+	height: 300px;
+	background: blue;
+	color: #fff;
+}
+</style>
--- src/views/Content/index.vue
+++ src/views/Content/index.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="content">
-		Content Area
+		<h2>Dynamic Data</h2>
+		<div class="tab">
+			<div @click="switchCom(item)" :key="item.name" v-for="item in data" class="tab-item">
+				{{ item.name }}
+			</div>
+		</div>
+		<component :is="currentComponent.comName"></component>
+		<h2>Static Data</h2>
 		<div :key="index" v-for="(item, index) in dataList" class="content-item">
 			<Card :title="item.label" :content="`$${item.price}`"></Card>
 		</div>
@@ -8,6 +15,33 @@
 </template>
 	
 <script setup lang='ts'>
+import Apple from '@/components/Dynamics/Apple.vue'
+import Butter from '@/components/Dynamics/Butter.vue'
+import Cat from '@/components/Dynamics/Cat.vue'
+import { reactive, markRaw, shallowReactive } from 'vue';
+type Tabs = {
+	name: string
+	comName: any
+}
+const data = reactive<Tabs[]>([
+	{
+		name: '苹果',
+		comName: markRaw(Apple)
+	},
+	{
+		name: '黄油',
+		comName: markRaw(Butter)
+	},
+	{
+		name: '猫',
+		comName: markRaw(Cat)
+	}
+])
+type Com = Pick<Tabs, 'comName'>
+const currentComponent = reactive<Com>({
+	comName: data[0].comName
+})
+
 type Product = {
 	label: string
 	des?: string
@@ -18,9 +52,25 @@ const dataList: Product[] = [
 	{ label: 'Bread', des: 'wadawdawdwadwadwdwad', price: 32.3 },
 	{ label: 'Gin', des: 'qweqweqweqweadadwddawdwa', price: 12.3, domestic: true },
 ]
+
+const switchCom = (item: Tabs) => {
+	console.log('item', item)
+	currentComponent.comName = item.comName
+}
+
 </script>
 
 <style lang="scss" scoped>
+.tab {
+	display: flex;
+
+	.tab-item {
+		margin: 10px;
+		border: 1px solid #666;
+		padding: 5px;
+	}
+}
+
 .content {
 	width: calc(100% - 20px);
 	height: calc(100% - 100px);
