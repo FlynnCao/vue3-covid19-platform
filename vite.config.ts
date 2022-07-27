735e800521d26a4ca5eb6c525f4ffd30e972f96f
Flynn <ppstream123@126.com>
2022-07-27 15:05:47 +0800

    🐳 chore: unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueJSX from "@vitejs/plugin-vue-jsx";
 import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
 const pathResolve = (dir: string) => resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueJSX()],
+  plugins: [
+    vue(),
+    VueJSX(),
+    AutoImport({
+      imports: ["vue"],
+      dts: "src/auto-import.d.ts",
+    }),
+  ],
   resolve: {
     alias: {
       "@": pathResolve("./src"),
